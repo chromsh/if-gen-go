304c25dbf1c7c7e8dbf5339dfc17a01d4147c34d
chrom <chrom@chromsh.com>
2021-09-25 01:08:44 +0900

    add decoding utility

---
+++ compiler/codec.go
@@ -0,0 +1,56 @@
+package compiler
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+type Decoder struct {
+	data   []byte
+	cursor int
+
+	zero []byte
+	one  []byte
+}
+
+func NewDecoder(data []byte) Decoder {
+	zero := " "
+	one := "if"
+	return Decoder{
+		data:   data,
+		cursor: 0,
+		zero:   []byte(zero),
+		one:    []byte(one),
+	}
+}
+
+// ReadBit read 1bit
+func (d *Decoder) ReadBit() (byte, error) {
+	if len(d.data) <= d.cursor {
+		return 0, io.EOF
+	}
+
+	remainBytes := len(d.data) - d.cursor
+	fmt.Printf("remain = %v, cursor = %v\n", remainBytes, d.cursor)
+	// check zero
+	if len(d.zero) <= remainBytes {
+		bs := d.data[d.cursor : d.cursor+len(d.zero)]
+		if bytes.Equal(d.zero, bs) {
+			fmt.Printf("cursor = %v, zero\n", d.cursor)
+			d.cursor += len(d.zero)
+			return 0, nil
+		}
+	}
+
+	// check one
+	if len(d.one) <= remainBytes {
+		bs := d.data[d.cursor : d.cursor+len(d.one)]
+		if bytes.Equal(d.one, bs) {
+			fmt.Printf("cursor = %v, one\n", d.cursor)
+			d.cursor += len(d.one)
+			return 1, nil
+		}
+	}
+	return 0, fmt.Errorf("illegal file format, cursor=%v", d.cursor)
+}
