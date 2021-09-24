015671416524f1653b386a13a28869390ec733fb
chrom <chrom@chromsh.com>
2021-09-25 01:08:00 +0900

    remove unexpected line feed

--- cmd/compile.go
+++ cmd/compile.go
@@ -18,18 +18,7 @@ var compileCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(compileCmd)
-	compileCmd.Flags().StringP("file", "f", "", "compile target file")
-	compileCmd.PersistentFlags().String("file", "", "A help for foo")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// compileCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// compileCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//compileCmd.Flags().StringP("file", "f", "", "compile target file")
 }
 
 func runCompile(fname string) error {
@@ -42,6 +31,6 @@ func runCompile(fname string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(res)
+	fmt.Print(res)
 	return nil
 }
